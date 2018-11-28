357f1aa9179ebc0af78a7838ab647b98e31430a3
Harindi Supunsara <supunsara.harindi@gmail.com>
2018-11-28 14:25:25 +0530

    Load all physical branches

--- src/main/java/com/arpico/groupit/usermanagement/service/BranchService.java
+++ src/main/java/com/arpico/groupit/usermanagement/service/BranchService.java
@@ -1,8 +1,11 @@
 package com.arpico.groupit.usermanagement.service;
 
+import java.util.List;
+
 public interface BranchService {
 
 	String getBranch(String userName) throws Exception;
 	
+	List<String> getAllPhysicalBranches() throws Exception;
 
 }
--- src/main/java/com/arpico/groupit/usermanagement/service/impl/BranchServiceImpl.java
+++ src/main/java/com/arpico/groupit/usermanagement/service/impl/BranchServiceImpl.java
@@ -1,12 +1,15 @@
 package com.arpico.groupit.usermanagement.service.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.arpico.groupit.usermanagement.dao.BranchDao;
 import com.arpico.groupit.usermanagement.dao.SysUserDao;
+import com.arpico.groupit.usermanagement.model.BranchModel;
 import com.arpico.groupit.usermanagement.model.SysUserBranchModel;
 import com.arpico.groupit.usermanagement.model.SysUserModel;
 import com.arpico.groupit.usermanagement.service.BranchService;
@@ -17,6 +20,9 @@ public class BranchServiceImpl implements BranchService{
 
 	@Autowired
 	private SysUserDao sysUserDao;
+	
+	@Autowired
+	private BranchDao branchDao;
 	
 	@Override
 	public String getBranch(String userName) throws Exception {
@@ -34,6 +40,19 @@ public class BranchServiceImpl implements BranchService{
 		
 		
 		return branchs; //sysUserModel.getBranch().getCode();
+	}
+
+	@Override
+	public List<String> getAllPhysicalBranches() throws Exception {
+		
+		List<BranchModel> branchModels=branchDao.findByPhysical("1");
+		List<String> branchcodes=new ArrayList<>();
+		branchModels.forEach(branch -> {
+			
+			branchcodes.add(branch.getCode());
+		});
+		
+		return branchcodes;
 	}
 
 }
